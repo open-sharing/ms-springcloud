ae1f0e03dd7825b117bfd8094f549276941662dc
wzw <304807481@qq.com>
2017-11-30 10:16:48 +0800

    初始化: eureka服务注册服务

---
+++ eureka-server/src/main/java/com/ms/springcloud/Application.java
@@ -0,0 +1,20 @@
+package com.ms.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册主类
+ *
+ * @author jwen
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
