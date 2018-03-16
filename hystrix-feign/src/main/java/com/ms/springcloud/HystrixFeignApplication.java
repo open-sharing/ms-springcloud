b55f9ea1d372ead31c4c5c3742069a4f9d665eda
wzw <304807481@qq.com>
2018-03-16 16:53:11 +0800

    feign项目支持端点/hystrix.stream

--- hystrix-feign/src/main/java/com/ms/springcloud/HystrixFeignApplication.java
+++ hystrix-feign/src/main/java/com/ms/springcloud/HystrixFeignApplication.java
@@ -2,6 +2,7 @@ package com.ms.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
@@ -10,10 +11,9 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
  *
  * [关键代码]
  *   1. 依赖spring-cloud-starter-hystrix, 依赖spring-cloud-starter-feign
- *   2. 启动类上额外加上@EnableCircuitBreaker(或@EnableHystrix)
- *   3. UserFeignClientFallback上@Component
- *   4. 声明式调用服务的接口类 @FeignClient(name = "producer", fallback = UserFeignClientFallback.class)
- *   5. [坑]配置文件:feign.hystrix.enabled=true
+ *   2. UserFeignClientFallback上@Component
+ *   3. 声明式调用服务的接口类 @FeignClient(name = "producer", fallback = UserFeignClientFallback.class)
+ *   4. [坑]配置文件:feign.hystrix.enabled=true
  *      参照:http://blog.csdn.net/qq_20079921/article/details/77990935?locationNum=7&fps=1
  *
  * @author jwen
@@ -21,6 +21,8 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
+//为了支持端点/hystrix.stream, 需要增加@EnableCircuitBreaker和spring-cloud-starter-hystrix依赖
+@EnableCircuitBreaker
 public class HystrixFeignApplication {
 
     public static void main(String[] args) {
